cc54e4e9c1f4500d0efa5bf0145d006d41885a31
Eliza Reed <ereed@madeira.org>
2020-09-17 18:10:19 -0400

    final commit

--- src/madeira/cs/Week3Challenge.java
+++ src/madeira/cs/Week3Challenge.java
@@ -49,6 +49,7 @@ public class Week3Challenge {
             System.out.println ("Same!");
         }
         else if (musicGenre.equals ("Indie pop")) {
+            System.out.println ("Same!");
         }
         else {
             System.out.println("Lame.");
@@ -65,12 +66,11 @@ public class Week3Challenge {
 
         Scanner guessScanner = new Scanner (System.in);
         int number = 103;
-        int limit = 10;
         int measurement, guess;
 
         System.out.println("I am thinking of an integer. You have 10 guesses to find it.");
 
-        for (measurement = 0; measurement < limit; measurement++) {
+        for (measurement = 0; measurement < 10; measurement++) {
             System.out.println("Guess the integer.");
 
             guess = guessScanner.nextInt();
@@ -80,15 +80,15 @@ public class Week3Challenge {
                 System.out.println("You have successfully guessed the number.");
                 break;
             }
-            else if (number > guess && measurement != limit - 1) {
+            else if (number > guess && measurement != 10 - 1) {
                 System.out.println("Too small! Try again.");
             }
-            else if (number < guess && measurement != limit - 1) {
+            else if (number < guess && measurement != 10 - 1) {
                 System.out.println("Too big! Try again.");
             }
         }
 
-        if (measurement == limit) {
+        if (measurement == 10) {
             System.out.println("You have failed to guess the number.");
             System.out.println ("How does it feel, being a failure? I wouldn't know.");
             System.out.println ("By the way, your mother was a hamster and your father smelt of elderberries.");
@@ -97,7 +97,129 @@ public class Week3Challenge {
             }
 
 
-        
+/*
+    Problem IV
+    ----------
+            Produce the following output using one print statement
 
+        *****
+        ****
+        ***
+        **
+        *
+*/
+        int outerCounter = 5;
+        while (outerCounter > 0) {
+            System.out.println();
+            int innerCounter = 0;
+            while (innerCounter < outerCounter) {
+                System.out.print ("*");
+                innerCounter++;
+            }
+            outerCounter--;
+        }
+        System.out.println ();
+/*
+    Problem V
+    ---------
+            Produce the following output using one print statement
+
+        ****
+        **
+        *
+*/
+
+        int outerCounter2 = 4;
+        while (outerCounter2 > 0) {
+            System.out.println();
+            int innerCounter2 = 0;
+            while (innerCounter2 < outerCounter2) {
+                System.out.print ("*");
+                innerCounter2++;
+            }
+            outerCounter2/= 2;
+        }
+        System.out.println ();
+
+/*
+    ------------------
+            EXTRA CREDIT BELOW
+    ------------------
+
+
+            Problem VI
+            ----------
+    Declare an number, any number.
+            Prompt the user to guess your number.
+    Give them 10 tries to get it right.
+            For each try they should either be:
+    Hot: +/- 2
+    Medium: +/- 4
+    Mild: +/- 8
+    If they are Hot they should have 4 guesses subtracted from their total remaining
+    If they are Medium they should have 2 guesses subtracted from their total remaining
+    If they are Mild they should have 1 guess subtracted from their total remaining
+
+    Once they're out of guesses, they should be prompted to play again.
+
+
+    Edge Cases:
+    - If I have 4 guesses remaining and my guess is within 2 of the correct answer I should receive the prompt: "One more guess!"
+            - Same goes for 2 guesses remaining with a Medium guess
+    - User should be able to enter ANY TYPE of number
+
+    Brownie points for well commented / organized code that goes above and beyond to direct the user clearly & leaves no room for ambiguous edge cases
+
+*/
+        System.out.println ("I am thinking of a number.");
+        System.out.println ("You have ten tries to guess it correctly, but be careful - your guesses will decrease when you get closer to the number I'm thinking of.");
+        System.out.println ("Your time starts...");
+        System.out.println ("...NOW!");
+
+        // Declaring my number
+        double secret = 42;
+
+        // Creating a loop for the guessing
+        int counter = 0;
+        while (counter < 10) {
+            // Creating a scanner for them to enter their guess.
+            Scanner myScanner = new Scanner (System.in);
+            double guess2 = myScanner.nextDouble();
+
+            // If they guess the number, they get a congratulations message and the loop breaks.
+            if (guess2 == secret) {
+                System.out.println("Congratulations! You have guessed the secret to life, the universe, and everything.");
+                break;
+            }
+            // If their number is within two of the secret number, they lose 4 guesses.
+            else if (guess2 >= 40 && guess2 <= 44 && counter != 10 - 1) {
+                System.out.println ("HOT!!");
+                if (counter == 4) System.out.println ("One more guess!");
+                counter += 4;
+            }
+            // If their number is within four of the secret number, they lose 2 guesses.
+            else if (guess2 >= 38 && guess2 <= 46 && counter != 10 - 1) {
+                System.out.println ("Medium!");
+                if (counter == 2) System.out.println ("One more guess!");
+                counter += 2;
+            }
+            // If their number is within eight of the secret number, they lose 1 guess.
+            else if (guess2 >= 34 && guess2 <= 50 && counter != 10 - 1) {
+                System.out.println ("Eh. Mild.");
+                counter++;
+            }
+            // And if their number isn't even within eight of the secret number, they still lose one guess.
+            else if (counter != 10 - 1) {
+                System.out.println ("Not even close.");
+                counter++;
+            }
+            // When counter reaches 10, they get this failure message.
+            if (counter >= 10) {
+                System.out.println ("OOPS, you're out of guesses.");
+                System.out.println ("How could this happen?");
+                System.out.println ("I'll give you a hint to the answer, though.");
+                System.out.println ("It's the answer to life, the universe, and everything.");
+                break;
+            } }
     }
 }
